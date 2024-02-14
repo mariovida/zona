21baf751b033271d9dd9eb2325fe936884056cbe
Mario <mariovidovic97@gmail.com>
2024-02-14 09:25:10 +0100

    project single page

--- project/azure-waters-apartments.php
+++ project/azure-waters-apartments.php
@@ -28,6 +28,38 @@
         <img src="../images/project_2.jpg" />
     </section>
 
+     <section class="project_info">
+        <div class="wrapper">
+            <div class="row" style="--bs-gutter-x:4rem">
+                <div class="col-4">
+                    <table>
+                        <tr>
+                            <td class="pt-0">Location</td>
+                            <td class="pt-0 light-color">Liverpool, UK</td>
+                        </tr>
+                        <tr>
+                            <td>Date</td>
+                            <td class="light-color">2021</td>
+                        </tr>
+                        <tr>
+                            <td>Value</td>
+                            <td class="light-color">€ 71.000,00</td>
+                        </tr>
+                        <tr>
+                            <td>Service</td>
+                            <td class="light-color">Interior Design</td>
+                        </tr>
+                        <tr>
+                            <td>Size</td>
+                            <td class="light-color">109 m<sup>2</sup></td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="col-8">B</div>
+            </div>
+        </div>
+    </section>
+
     <?php
         define('footer', TRUE);
         include 'inc/footer.php';
--- project/harmony-park-residence.php
+++ project/harmony-park-residence.php
@@ -28,6 +28,38 @@
         <img src="../images/project_1.jpg" />
     </section>
 
+    <section class="project_info">
+        <div class="wrapper">
+            <div class="row" style="--bs-gutter-x:4rem">
+                <div class="col-4">
+                    <table>
+                        <tr>
+                            <td class="pt-0">Location</td>
+                            <td class="pt-0 light-color">Las Vegas, USA</td>
+                        </tr>
+                        <tr>
+                            <td>Date</td>
+                            <td class="light-color">2019</td>
+                        </tr>
+                        <tr>
+                            <td>Value</td>
+                            <td class="light-color">€ 25.000,00</td>
+                        </tr>
+                        <tr>
+                            <td>Service</td>
+                            <td class="light-color">Design & Planning</td>
+                        </tr>
+                        <tr>
+                            <td>Size</td>
+                            <td class="light-color">227 m<sup>2</sup></td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="col-8">B</div>
+            </div>
+        </div>
+    </section>
+
     <?php
         define('footer', TRUE);
         include 'inc/footer.php';
