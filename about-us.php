54ede910aa2728e87c033fdbe373414f3026f986
Mario <mariovidovic97@gmail.com>
2024-02-25 09:25:03 +0100

    experties added

--- about-us.php
+++ about-us.php
@@ -48,6 +48,89 @@
                     <h3>Our Experties</h3>
                 </div>
             </div>
+            <div class="row" style="--bs-gutter-x:6rem">
+                <div class="col-6">
+                    <div class="experties-image">
+                        <img src="./images/experties_1.jpg" />
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="accordion">
+                        <div class="accordion-item">
+                            <div class="accordion-link" style="padding-top:0">
+                                <h5>Interior Design</h5>
+                                <img src="./assets/arrow-right.svg" />
+                            </div>
+                            <div class="answer">
+                                <p>We understand that the interior of a building is just as important as its exterior. Our team of experienced interior designers specializes in creating functional and aesthetically pleasing spaces that reflect our clients' unique needs and preferences. From conceptualization to execution, we work closely with our clients to ensure that every detail is tailored to their vision. Whether it's a residential, commercial, or institutional project, we bring creativity, innovation, and attention to detail to every interior design project we undertake.</p>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <div class="accordion-link">
+                                <h5>Architecture</h5>
+                                <img src="./assets/arrow-right.svg" />
+                            </div>
+                            <div class="answer">
+                                <p>Architecture is not just about creating buildings - it's about shaping experiences and transforming spaces. Our team of talented architects combines creativity, innovation, and technical expertise to design buildings that are both functional and aesthetically pleasing. From concept to completion, we work closely with our clients to bring their vision to life, creating spaces that inspire and delight. Whether it's a residential, commercial, or institutional project, we approach every design with the same level of dedication and passion, ensuring that each building is a true reflection of our client's needs and aspirations.</p>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <div class="accordion-link">
+                                <h5>Landscape Design</h5>
+                                <img src="./assets/arrow-right.svg" />
+                            </div>
+                            <div class="answer">
+                                <p>We believe that the exterior of a building is just as important as its interior. Our landscape designers are passionate about creating outdoor spaces that are both beautiful and functional. From lush gardens and serene courtyards to dynamic public spaces and sustainable landscapes, our team brings creativity, innovation, and expertise to every project. Whether it's a residential garden, a commercial development, or a public park, we work closely with our clients to design outdoor environments that enhance the overall experience and harmony of a project.</p>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <div class="accordion-link">
+                                <h5>Exterior Design</h5>
+                                <img src="./assets/arrow-right.svg" />
+                            </div>
+                            <div class="answer">
+                                <p>We understand that the exterior of a building is the first thing people see and sets the tone for the entire project. Our team of experienced architects and designers specializes in creating visually stunning and functional exteriors that capture the essence of the project. From modern and minimalist to traditional and ornate, our designs are tailored to meet the unique needs and preferences of our clients. Whether it's a residential, commercial, or institutional project, we bring creativity, innovation, and attention to detail to every exterior design project we undertake.</p>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <div class="accordion-link">
+                                <h5>Consultation</h5>
+                                <img src="./assets/arrow-right.svg" />
+                            </div>
+                            <div class="answer">
+                                <p>Our experienced team of architects, designers, and project managers work closely with clients to understand their unique needs, objectives, and constraints. We offer expert advice on a wide range of topics, including project feasibility, budgeting, regulatory compliance, sustainability, and more. Whether you're looking for guidance on a new project or seeking support for an ongoing one, our consultation services are designed to help you make informed decisions and achieve your goals.</p>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <div class="accordion-link">
+                                <h5>Design & Planning</h5>
+                                <img src="./assets/arrow-right.svg" />
+                            </div>
+                            <div class="answer">
+                                <p>The key to a successful project lies in thoughtful and strategic design and planning. Our experienced team works closely with clients to develop creative and innovative solutions that are tailored to their unique needs and objectives. From initial concept development to detailed planning and execution, we offer a comprehensive range of design and planning services, including architectural design, interior design, master planning, urban design, and landscape design. Whether it's a small-scale renovation or a large-scale development, we bring creativity, expertise, and attention to detail to every project.</p>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <div class="accordion-link">
+                                <h5>Sustainability & Green Design</h5>
+                                <img src="./assets/arrow-right.svg" />
+                            </div>
+                            <div class="answer">
+                                <p>Sustainability is at the core of our design philosophy at [Company Name]. Our team of architects, designers, and engineers are committed to creating environmentally friendly and energy-efficient buildings and spaces. We incorporate sustainable design principles into every project, from energy-efficient building materials to passive design strategies. Our goal is to create buildings that not only meet the needs of our clients but also contribute positively to the environment and the communities in which they are located.</p>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <div class="accordion-link">
+                                <h5>Accessibility & Universal Design</h5>
+                                <img src="./assets/arrow-right.svg" />
+                            </div>
+                            <div class="answer">
+                                <p>Good design should be accessible to everyone and that's why we incorporate universal design principles into every project, from barrier-free entrances and accessible restrooms to tactile signage and wayfinding systems. Our goal is to create spaces that are not only beautiful and functional but also welcoming and inclusive for everyone.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 
@@ -55,7 +138,7 @@
         define('footer', TRUE);
         include 'inc/footer.php';
     ?>
-    
+
     <script>
         const observer = new IntersectionObserver((entries) => {
             entries.forEach((entry) => {
@@ -67,6 +150,15 @@
 
         const hiddenElements = document.querySelectorAll('.hide-image');
         hiddenElements.forEach((el) => observer.observe(el));
+
+        $(".accordion-link").click(function() {
+            const parentItem = $(this).closest('.accordion-item');
+            const isOpen = parentItem.hasClass('active');
+            $('.accordion-item').removeClass('active');
+            if (!isOpen) {
+                parentItem.addClass('active');
+            }
+        });
     </script>
 </body>
 </html>
