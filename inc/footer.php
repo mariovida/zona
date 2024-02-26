02482e2d57aabff0ad3d2c5537ddec7f402423e3
Mario <mariovidovic97@gmail.com>
2024-02-26 08:25:14 +0100

    footer style

--- inc/footer.php
+++ inc/footer.php
@@ -2,4 +2,48 @@
     defined('footer') or header('Location: ../');   
 ?>
 <footer>
+    <div class="wrapper">
+        <div class="row">
+            <div class="col-6 d-flex flex-column align-items-start">
+                <img src="./assets/logo.svg" class="footer_logo" />
+                <span>© 2024 Zona.</span>
+            </div>
+            <div class="col-2">
+                <div class="footer_links d-flex flex-column">
+                    <p>What We Do</p>
+                    <a href="">Interior Design</a>
+                    <a href="">Architecture</a>
+                    <a href="">Landscape Design</a>
+                    <a href="">Consultation</a>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="footer_links d-flex flex-column">
+                    <p>Pages</p>
+                    <a href="./about-us">About us</a>
+                    <a href="./projects">Projects</a>
+                    <a href="./services">Services</a>
+                    <a href="">Blogs</a>
+                    <a href="">FAQ</a>
+                    <a href="./contact">Contact us</a>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="footer_links d-flex flex-column">
+                    <p>Resources</p>
+                    <a href="">Privacy Policy</a>
+                    <a href="">Terms & Conditions</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer_border"></div>
+    <div class="wrapper">
+        <div class="row">
+            <div class="col-12 d-flex align-items-center justify-content-between">
+                <h6>Built by MV.</h6>
+                <a href="https://github.com/mariovida" target="_blank"><img src="./assets/github.svg" class="git-btn" /></a>
+            </div>
+        </div>
+    </div>
 </footer>
